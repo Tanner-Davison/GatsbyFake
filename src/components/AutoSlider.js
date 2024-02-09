3cadf509870e303f6b724f502f9115015e55244c
Tanner Davison <Tboydavison@gmail.com>
2024-02-09 01:20:44 -0700

    added the mobile/desktop/tablet/fullwidth

--- src/components/AutoSlider.js
+++ src/components/AutoSlider.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState,useRef} from "react"
 import styled from "styled-components"
 import {autoSliderData} from "../components/content/AutoSliderData"
 import colors from "../styles/colors"
@@ -6,39 +6,97 @@ import media from "../styles/media"
 import {gsap} from "gsap"
 import text from "../styles/text"
 import {SliderHeader} from "../components/content/AutoSliderData"
-import getMedia from "../utils/getMedia"
+import useMedia from "../utils/useMedia"
 const AutoSlider = ({scrollto}) => {
+  let windowMobile = window.innerWidth <= 481
+
+  const [counter, setCounter] = useState(1)
+  // let ref = useRef(0)
+  // const startInterval =()=> setInterval(()=>{
+    
+  //   setCounter(prev=> prev+1<10 ? prev+1 : 0);
+   
+  // },4000)
+  const startMobileGsap = () => {
+    let mobileLoaders = document.querySelectorAll(".growme")
+    let mobileTargets = document.querySelectorAll(".boxself")
+    let length = mobileLoaders.length
+    const tlM = gsap.timeline({
+      paused: false,
+      repeat: -1,
+    })
+    console.log(mobileTargets.length)
+    // startInterval();
+    for (let i = 1; i < mobileTargets.length + 1; i++) {
+      let count = i
+      console.log(count)
+      if (count === mobileTargets.length) {
+        tlM
+          .to(".boxself", {xPercent: 0, duration: 1})
+      } else {
+        tlM.to(".growme.num1", {width:100, duration: 5, ease:'power1.easeInOut'})
+        tlM.to(".growme.num1", { width: 0, duration:0})
+        tlM.add(function () {
+          setCounter(prev => (prev < 9 ? prev + 1 : 0))
+        },'-=2.2')
+        tlM
+          .to(
+            ".boxself",
+            {
+              xPercent: count === 1 ? -106.8 : -106.8 * count,
+              duration: 1.5,
+              ease: "expo.inOut",
+            },
+            "-=2.5"
+          )
+          
+      }
+      
+    }
+    // return () => clearInterval(startInterval)
+  }
+
   const startGsap = () => {
     let loadSpeed = 7
     let slideSpeed = 1.5
     const targetDiv = document.querySelectorAll(`.boxself`)
     const tl = gsap.timeline({
       paused: false,
-      repeat:-1,
-      
+      repeat: -1,
     })
-    
-    tl.to(".growme.num1", {width: 100, duration: `${loadSpeed }`})
-    tl.to('.boxself',{xPercent: -335, duration: `${slideSpeed }`, ease: "back.inOut"},"-=4")
-    tl.to(".growme.num2", {width: 100, duration: `${loadSpeed}`,ease:'smooth'}, "-=3")
+
+    tl.to(".growme.num1", {width: 100, duration: `${loadSpeed}`})
+    tl.to(
+      ".boxself",
+      {xPercent: -335, duration: `${slideSpeed}`, ease: "back.inOut"},
+      "-=4"
+    )
+    tl.to(
+      ".growme.num2",
+      {width: 100, duration: `${loadSpeed}`, ease: "smooth"},
+      "-=3"
+    )
     tl.to(
       targetDiv,
       {xPercent: -670, duration: `${slideSpeed}`, ease: "back.inOut"},
       "-=4"
     )
-    tl.to(".growme.num3", {width: 100, duration: `${loadSpeed}`, ease:'smooth'}, "-=3")
+    tl.to(
+      ".growme.num3",
+      {width: 100, duration: `${loadSpeed}`, ease: "smooth"},
+      "-=3"
+    )
     tl.to(
       targetDiv,
       {xPercent: 0, duration: `${slideSpeed}`, ease: "back.inOut"},
       "-=4"
     )
-    
-  
   }
   useEffect(() => {
-    const target = document.getElementById(`#cardwrapper`)
+    const target = document.getElementById(`cardwrapper`)
     gsap.set(target, {xPercent: 0})
-    startGsap()
+    windowMobile && startMobileGsap()
+    !windowMobile && startGsap()
   }, [])
 
   const runCards = (imgObj, index) => {
@@ -67,18 +125,35 @@ const AutoSlider = ({scrollto}) => {
         <ButtonCustom>
           <ButtonGrowth className={"growme num1"} />
         </ButtonCustom>
-        <ButtonCustom>
-          <ButtonGrowth className={"growme num2"} />
-        </ButtonCustom>
-        <ButtonCustom>
-          <ButtonGrowth className={`growme num3`} />
-        </ButtonCustom>
+
+        {!windowMobile && (
+          <>
+            <ButtonCustom>
+              <ButtonGrowth className={"growme num2"} />
+            </ButtonCustom>
+            <ButtonCustom>
+              <ButtonGrowth className={`growme num3`} />
+            </ButtonCustom>
+          </>
+        )}
       </Controls>
+      {windowMobile && (
+        <Counter>
+          {counter} / {autoSliderData.length - 1}{" "}
+        </Counter>
+      )}
     </Wrapper>
   )
 }
 
 export default AutoSlider
+const Counter = styled.p`
+  position: relative;
+  ${text.bodyM}
+  padding-top:0.903vw;
+  margin:unset;
+  color:white;
+`
 const ButtonGrowth = styled.div`
   position: relative;
   display: flex;
@@ -96,6 +171,8 @@ const ButtonGrowth = styled.div`
   }
 
   ${media.mobile} {
+    
+    
   }
 `
 const ButtonCustom = styled.div`
@@ -117,9 +194,11 @@ const ButtonCustom = styled.div`
   }
 
   ${media.mobile} {
+    width: 13.785vw;
   }
 `
 const Controls = styled.div`
+position: relative;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -166,6 +245,8 @@ const Image = styled.div`
   }
 
   ${media.mobile} {
+    width: 46.262vw;
+    height: 60.28vw;
   }
 `
 //////Wrapper for all content being animated
@@ -184,11 +265,15 @@ const Card = styled.div`
   height: 29.653vw;
   width: 24.583vw;
   padding: 1.667vw 1.667vw 3.75vw 1.389vw;
+  box-shadow: 0vw 0.278vw 0.278vw 0vw rgba(0, 0, 0, 0.25);
+  border: 0.069vw solid #fff;
+  backdrop-filter: blur(1px);
   ${media.fullWidth} {
     border-radius: 24px;
     height: 427px;
     width: 354px;
     padding: 24px 24px 54px 20px;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
   }
 
   ${media.tablet} {
@@ -198,6 +283,11 @@ const Card = styled.div`
   }
 
   ${media.mobile} {
+    width: 82.71vw;
+    height: 93.458vw;
+    padding: 24px 24px 24px 20px;
+    border-radius: 5.607vw;
+    box-shadow: 0px 1px 1px 0px rgba(0, 0, 0, 0.25);
   }
 `
 const CardRelativeWrapper = styled.div`
@@ -213,6 +303,7 @@ const CardRelativeWrapper = styled.div`
   }
 
   ${media.mobile} {
+    gap: 22px;
   }
 `
 ////////////////////////////
@@ -233,12 +324,10 @@ const ViewBox = styled.div`
 
   ${media.tablet} {
     width: 94.188vw;
-    
   }
 
   ${media.mobile} {
-    width: unset;
-    height: unset;
+    width: 82.71vw;
     border-radius: 5.607vw;
   }
 `
@@ -284,6 +373,6 @@ const Wrapper = styled.div`
   }
 
   ${media.mobile} {
-    padding: 14.019vw 9.346vw;
+    padding: 9.346vw 6.075vw 8.178vw 6.075vw;
   }
 `
